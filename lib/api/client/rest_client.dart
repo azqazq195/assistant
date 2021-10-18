0ccd9f02f15b62afcefd9f89476e16d34f91e2bc
moseoh <azqazq195@gmail.com>
2021-10-18 23:41:50 +0900

    token 숨김 처리

--- lib/api/client/rest_client.dart
+++ lib/api/client/rest_client.dart
@@ -1,6 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:assistant/constants/token.dart';
 
 part 'rest_client.g.dart';
 
@@ -8,13 +9,11 @@ part 'rest_client.g.dart';
 abstract class RestClient {
   factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
 
-  @Headers({"Accept": "application/vnd.github.v3+json",
-    "Authorization": "token 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"})
+  @Headers({"Accept": "application/vnd.github.v3+json", "Authorization": token})
   @GET('/latest')
   Future<Release> getReleaseLatest();
 
-  @Headers({"Accept": "application/vnd.github.v3+json",
-  "Authorization": "token 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"})
+  @Headers({"Accept": "application/vnd.github.v3+json", "Authorization": token})
   @GET('')
   Future<List<Release>> getReleaseList();
 }
