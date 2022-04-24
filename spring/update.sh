0267335f2b8a508099b65f9676aca9772b5e0725
moseoh <azqazq195@gmail.com>
2022-04-24 14:17:55 +0900

    update

---
+++ spring/update.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+
+RED="\033[1;31""m"
+GREEN="\033[1;32""m"
+YELLOW="\033[1;33""m"
+BLUE="\033[1;34""m"
+PINK="\033[1;35""m"
+NC="\033[0m"
+
+echo "${GREEN} > commit comment: ${NC}"
+read COMMIT
+
+git pull origin master
+git add .
+git commit -m "$COMMIT"
+git push origin master
+
+echo "${GREEN} > build docker image ${NC}"
+./gradlew bootBuildImage --imageName=ID/Repository
