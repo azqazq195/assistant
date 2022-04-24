2ed719fda55ae7a28fb1ff5562c1506f77753b5b
moseoh <azqazq195@gmail.com>
2022-04-24 14:30:11 +0900

    update

--- spring/update.sh
+++ spring/update.sh
@@ -13,10 +13,25 @@ read COMMIT
 git pull origin master
 git add .
 git commit -m "$COMMIT"
+
+echo "${GREEN} > git push.. ${NC}"
+
 git push origin master
 
-echo "${GREEN} > build docker image ${NC}"
-echo "${GREEN} > docker image tag: ${NC}"
-read TAG
+# echo "${GREEN} > build docker image.. ${NC}"
+# echo "${GREEN} > docker image tag: ${NC}"
+# read TAG
 
-./gradlew bootBuildImage -Dspring.profiles.active=dev --imageName=azqazq195/assistant:"$TAG"
+# ./gradlew bootBuildImage -Dspring.profiles.active=dev --imageName=azqazq195/assistant:"$TAG"
+
+# echo "${GREEN} > docker push.. ${NC}"
+
+# docker push azqazq195/assistant:"$TAG"
+
+echo "${GREEN} > build docker image.. ${NC}"
+
+./gradlew bootBuildImage -Dspring.profiles.active=dev --imageName=azqazq195/assistant
+
+echo "${GREEN} > docker push.. ${NC}"
+
+docker push azqazq195/assistant
