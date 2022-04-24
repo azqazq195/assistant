8615577b3e2e3c5a90e6ee941b71956fbd37448e
moseoh <azqazq195@gmail.com>
2022-04-24 16:00:57 +0900

    dev port 변경

---
+++ spring/deploy.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+GREEN="\033[1;32""m"
+NC="\033[0m"
+
+echo "${GREEN} > stop container ${NC}"
+docker stop assistant_server
+echo "${GREEN} > remove container ${NC}"
+docker rm assistant_server
+echo "${GREEN} > pull latest image ${NC}"
+docker pull azqazq195/assistant_server
+echo "${GREEN} > run container ${NC}"
+docker run -d -p 9090:9090 --name=assistant_server --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v assistant_server_data:/data azqazq195/assistant_server
