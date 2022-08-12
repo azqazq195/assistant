1a1d2f9ae155185dd7c0fe1b57575608f61ca636
SEONGHA MOON <azqazq195@gmail.com>
2022-08-12 22:50:48 +0900

    next init

---
+++ web/README.md
@@ -0,0 +1,19 @@
+### next 설치
+
+`npx create-next-app {AppName}`
+
+react react-dom next 가 함께 설치된다.
+
+```shell
+npm i zustand next-theme
+# d 옵션은 dev dependency
+npm i -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+```
+
+추가 설정
+https://tailwindcss.com/docs/guides/nextjs
+
+### next 실행
+
+`npm run dev`
---
+++ web/components/layout.js
@@ -0,0 +1,14 @@
+import Header from './header';
+import Footer from './footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="bg-primary">
+      <Header />
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+        {children}
+      </section>
+      <Footer />
+    </div>
+  );
+}
